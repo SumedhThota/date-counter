6238b2076e0667622dd79199ef7e66dab9562a71
SumedhThota <sumedharya569@gmail.com>
2024-07-10 18:20:28 -0400

    Updated

--- src/App.js
+++ src/App.js
@@ -7,38 +7,38 @@ function App() {
   const [step, setStep] = useState(1)
   const [count, setCount] = useState(0)
 
+
   date.setDate(date.getDate()+count)
 
-
-  function handlePosStep(){
-    setStep((s)=>s+1)
-  }
-  function handleNegStep(){
-    setStep((s)=>s-1)
-  }
   function handlePosCount(){
     setCount((c)=>c+step)
   }
   function handleNegCount(){
     setCount((c)=>c-step)
   }
+  function handleReset(){
+    setCount(0)
+    setStep(1)
+  }
+
   return (
     <div className="App">
       <div>
+      <input type='range' min='0' max='10' value={step} onChange={e=> setStep(Number(e.target.value))}/>
       <div>
-        <button onClick={handleNegStep}>-</button>
-        <span> Step: {step}</span>
-        <button onClick={handlePosStep}>+</button>
+        <span> Step: {step}</span>      
       </div>
       <div>
-        <button onClick={handleNegCount}>-</button>
-        <span> Count: {count}</span>
+      <button onClick={handleNegCount}>-</button>
+        <input type='text' value ={count} onChange={e=>setCount(Number(e.target.value))}></input>      
         <button onClick={handlePosCount}>+</button>
       </div>
       </div>
       <p><span>{count === 0?"Today is":count>0?`${count} day from today is`:`${Math.abs(count)} days ago was`}</span>
       <span> {date.toDateString()}</span>
       </p>
+      {(count!== 0 || step!== 1)?<button onClick={handleReset}>Reset</button>: null }
+      
     </div>
   );
 }
